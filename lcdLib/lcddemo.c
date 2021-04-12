e6e3d6bd313c33aa736f04f2d4f3455889abf267
student <vherrera@miners.utep.edu>
2021-04-11 20:12:36 -0600

    First Push

--- lcdLib/lcddemo.c
+++ lcdLib/lcddemo.c
@@ -16,8 +16,28 @@ main()
 
   clearScreen(COLOR_BLUE);
 
-  drawString5x7(20,20, "hello", COLOR_GREEN, COLOR_RED);
+  
 
   fillRectangle(30,30, 60, 60, COLOR_ORANGE);
+  void drawChar5x7(u_char rcol, u_char rrow, char c, 
+     u_int fgColorBGR, u_int bgColorBGR) 
+{
+  u_char col = 0;
+  u_char row = 0;
+  u_char bit = 0x01;
+  u_char oc = c - 0x20;
+
+  lcd_setArea(rcol, rrow, rcol + 4, rrow + 7); /* relative to requested col/row */
+  while (row < 8) {
+    while (col < 5) {
+      u_int colorBGR = (font_5x7[oc][col] & bit) ? fgColorBGR : bgColorBGR;
+      lcd_writeColor(colorBGR);
+      col++;
+    }
+    col = 0;
+    bit <<= 1;
+    row++;
+  }
+}
   
 }
--- lcdLib/lcddemo2.c
+++ lcdLib/lcddemo2.c
@@ -16,7 +16,7 @@ main()
 
   clearScreen(COLOR_BLUE);
 
-  // drawString5x7(20,20, "hello", COLOR_GREEN, COLOR_RED);
+   drawString5x7(20,20, "Hello, Elizabeth", COLOR_GREEN, COLOR_RED);
 
   //  fillRectangle(30,30, 60, 60, COLOR_ORANGE);
   u_char offset_r = 60, offset_c = 60;
@@ -27,14 +27,32 @@ main()
     }*/
   for( int r = 0; r<=50; r++){
     for (int c = 0; c <=50-r; c++){
-      drawPixel(offset_c+c,offset_r+r,COLOR_GREEN);
       drawPixel(offset_c-c,offset_r+r,COLOR_GREEN);
+      drawPixel(offset_c+c,offset_r+r,COLOR_GREEN);
       
       
     }
     
     
+
   }
-  
-  
+  for (int r =offset_r; r<=offset_r-10; r++){
+    for( int c =offset_c;c<=offset_c -10; c++){ 
+    
+      drawPixel(offset_c+c,offset_r+r,COLOR_ORANGE);
+      
+    }
+  }
+  /* for (int r =0; r<=50; r++){
+    for (int c = 0; c<=r; c++){
+      drawPixel(offset_c -c, offset_r+r, COLOR_RED);
+    }
+  }
+  for ( int r = 0; r<=50; r++){
+    for ( int c =0; c<=r; c++){
+      drawPixel(offset_c+c, offset_r+r, COLOR_RED);
+      
+    }
+    }*/
+ 
 }
